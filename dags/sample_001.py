2eb22e11cd89d66594b727c5f4364789f4c804a5
Rodrigo Coelho <rodrigo.coelho@gruposbf.com.br>
2023-02-08 19:55:23 -0400

    chore: adjust dag to operator

--- dags/sample_001.py
+++ dags/sample_001.py
@@ -1,25 +1,33 @@
 from datetime import datetime
-from pendulum import timezone
+from os.path import dirname, join
+
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.bash import BashOperator
-from fisia.operators.pandas_to_bigquery import PandasToBigQueryOperator
+from fisia.financeiro.operators.pandas_to_bigquery import PandasToBigQueryOperator
+from pendulum import timezone
 
 with DAG(
     dag_id="sample_001",
     start_date=datetime(2023, 2, 1, tzinfo=timezone("America/Sao_Paulo")),
+    catchup=False,
     default_args={
         "owner": "analytics.engineering",
     },
     schedule_interval="@daily",
 ) as dag:
-
     jobs = {
         "start": EmptyOperator(task_id="start"),
         "stop": EmptyOperator(task_id="stop"),
     }
 
-    jobs["execute"] = PandasToBigQueryOperator(task_id="execute")
+    jobs["execute"] = PandasToBigQueryOperator(
+        task_id="execute",
+        source_code_path=join(dirname(__file__), "dataframe", "mycode.py"),
+        destination_table="refined.fis_ref_gld_dim_data",
+        location="US",
+        project_id="PROJETO AQUI",
+        write_mode="replace",
+    )
 
     jobs["execute"].set_upstream(jobs["start"])
     jobs["execute"].set_downstream(jobs["stop"])
